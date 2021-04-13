50e33ea616f2ff9892a6b0034fd0a4b454dfd77e
gmatei <mateicosmingoanta@gmail.com>
2021-04-13 17:32:15 +0300

    Authentication token system (first implementation)

--- app/user_service/src/main/java/com/example/user_service/controllers/UserController.java
+++ app/user_service/src/main/java/com/example/user_service/controllers/UserController.java
@@ -9,7 +9,8 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/public")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
